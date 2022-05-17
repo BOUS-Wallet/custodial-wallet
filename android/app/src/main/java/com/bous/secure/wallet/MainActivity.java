b0b8d0a9952a684d3e50bb34d21fb585ade8b039
atomauro <mauro.henaog@gmail.com>
2022-05-17 15:19:40 -0500

    .

---
+++ android/app/src/main/java/com/bous/secure/wallet/MainActivity.java
@@ -0,0 +1,58 @@
+package com.bous.secure.wallet;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.modules.core.DeviceEventManagerModule;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "BOUSWallet";
+  }
+
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+      return new ReactActivityDelegate(this, getMainComponentName()) {
+        @Override
+        protected ReactRootView createRootView() {
+          return new RNGestureHandlerEnabledRootView(MainActivity.this);
+        }
+      };
+  }
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    Intent intent = getIntent();
+    Uri uri = intent.getData();
+    if(uri != null){
+      ((MainApplication) getApplication()).uri = uri.toString();
+    }
+  }
+
+  @Override
+  public void onNewIntent(Intent intent) {
+    super.onNewIntent(intent);
+    Uri uri = intent.getData();
+    if(uri != null){
+      WritableMap params = Arguments.createMap();
+      params.putString("type", "onNewIntent");
+      params.putString("uri", uri.toString());
+      getReactInstanceManager().getCurrentReactContext()
+              .getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter.class)
+              .emit("onWalletConnectUri", params);
+    }
+  }
+}
